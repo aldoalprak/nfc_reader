44f4a71157852df40da98ba5746c92bfb7fd21f0
alprak <aldo.prakoso@taralite.com>
2019-08-07 20:15:12 +0700

    testing pckg

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 // import App from './App';
-import App from 'react-native-nfc-manager/example/AndroidMifareClassic'
+import App from 'react-native-nfc-manager/example/App'
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
